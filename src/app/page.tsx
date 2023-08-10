7b858ac7995a660661feea396260adb11146fae9
Joel Hernandez <you@example.com>
2023-08-10 07:29:42 -0400

    add dashboard layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import {redirect} from 'next/navigation';
 
-export default function Home() {
-  return (
-    <main>
-    
-    </main>
-  )
+export default function HomePage() {
+  redirect('/dashboard/main');
 }
