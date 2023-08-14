b56f228e81f23eb6991d3c52657250c02b5457b6
Joel Hernandez <you@example.com>
2023-08-14 08:17:02 -0400

    add avatar component

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,10 +1,14 @@
 import SearchBar from '@/components/SearchBar/SearchBar';
 import style from './main.module.css';
+import Avatar from '@/components/Avatar/Avatar';
+import avatarImage from '../../../assets/63352B2A-EB71-460B-BA27-0EA1F12A8EFF_1_105_c.jpeg';
+
 
 const page = () => {
   return (
     <section className={style.home_page}>
         <SearchBar/>
+        <Avatar name={'Joel Hernandez'} imageSrc={avatarImage} />
     </section>
   )
 }
