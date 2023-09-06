327610e7b5c8c3a002a072d8496e78891e7ea37b
Joel Hernandez <you@example.com>
2023-09-06 05:59:27 -0400

    make the bookmark icon to add or remove anime in the anime card component

--- src/components/AnimeList/AnimeCard.tsx
+++ src/components/AnimeList/AnimeCard.tsx
@@ -5,8 +5,7 @@ import Link from "next/link";
 import { useAppDispatch} from "@/store";
 import { shortnenText } from "@/helpers";
 import { AnimeListResponse } from "@/interfaces/animeListResponse";
-import { addAnime } from "@/store/anime/animeSlice";
-import {IoBookmarkOutline,IoBookmark,IoAddOutline } from 'react-icons/io5';
+import {IoBookmarkOutline,IoBookmark } from 'react-icons/io5';
 import AnimeRatingStars from "../AnimeRatingStars/AnimeRatingStars";
 import style from './animeList.module.css';
 import useAnimeListManager from "@/hooks/useAnimeListManager";
@@ -16,16 +15,19 @@ interface Props {
 }
 
 const AnimeCard = ({ anime }: Props) => {
-  const dispatch = useAppDispatch();
-  const {isAnimeStored} = useAnimeListManager();
+  const {isAnimeStored,addAnimeToList,removeAnimeFromList} = useAnimeListManager();
   const {_id,title,episodes,status,synopsis,image} = anime;
 
-  const toogleBookmark = () =>{
-    // dispatch(addAnime({_id,title,thumb,episodes,status}));
-  }
+ const handleClick = () =>{
+  addAnimeToList(anime);
+ }
+ const removeBookMarked = () =>{
+  removeAnimeFromList(_id);
+ }
+ 
   return (
-    <li className={style.anime_card}>
-      <Link href={`/dashboard/anime/${_id}`} onClick={toogleBookmark}>
+    <li className={style.anime_card} >
+   
       <Image
         className={style.anime_card_image}
         width={250}
@@ -36,8 +38,10 @@ const AnimeCard = ({ anime }: Props) => {
       <h3 className={style.anime_card_title}>{title}</h3>
       <p className={style.anime_card_status}>Status: <span className={style.anime_card_status_description}>{status}</span>
       </p>
+     
 
       <div className={style.anime_card_back}>
+       <Link href={`/dashboard/anime/${_id}`} >
         <p>{title}</p>
         <p className={style.anime_card_ranking}>
         <AnimeRatingStars
@@ -49,17 +53,15 @@ const AnimeCard = ({ anime }: Props) => {
         </p>
         <p>{episodes} Episodes</p>
         <p className={style.anime_card_synopsis}> {shortnenText(synopsis, 140)}</p>
-        
+        </Link>
         <div className={style.anime_card_icon_actions}>
-          
-        {!isAnimeStored(_id) ? <IoBookmarkOutline size={25}/>:<IoBookmark size={25}/>}  
-      
-      <IoAddOutline size={25} />
+         {!isAnimeStored(_id) 
+          ? <IoBookmarkOutline size={25} onClick={handleClick} />
+          : <IoBookmark size={25} onClick = {removeBookMarked}/>}  
+           
       </div>
-      
       </div>
-
-    </Link>
+  
     </li>
   )
 }
